da0351b188554ae08a3ca2a8ae0ab7c009644161
krox <krox@localhost>
2026-07-15 07:26:24 +0200

    Deploy the persistent graph workspace

--- web/dist/assets/react-force-graph-3d-DzSMjQLN.js
+++ web/dist/assets/react-force-graph-3d-DzSMjQLN.js
@@ -1,4 +1,4 @@
-import{i as _t,a as E_,f as xw,b as yw,c as vw,d as bw,e as Sw,t as aS,s as Tw,g as Mw,G as Ew,T as fx,E as px,h as ww,P as k}from"./EvidenceGraph-BM2Wxuto.js";import{g as Aw}from"./index-Btr-ZvKO.js";import{m as Rw,a as Cw,o as Nw}from"./string-BcQvp3wg.js";/**
+import{i as _t,a as E_,f as xw,b as yw,c as vw,d as bw,e as Sw,t as aS,s as Tw,g as Mw,G as Ew,T as fx,E as px,h as ww,P as k}from"./EvidenceGraph-B27YZk6T.js";import{g as Aw}from"./index-OS3tWHMA.js";import{m as Rw,a as Cw,o as Nw}from"./string-BcQvp3wg.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
